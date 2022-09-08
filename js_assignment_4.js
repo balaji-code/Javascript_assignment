4e32027c83e75c3b16341e87ab4a527cebacb765
balaji-code <111353789+balaji-code@users.noreply.github.com>
2022-09-08 11:16:23 +0530

    Javascript assignment 4

---
+++ js_assignment_4.js
@@ -0,0 +1,18 @@
+// to create a function that takes number of match houses and returns number of matches required
+
+function matchHouses(numHouses)
+{
+let matchSticks;
+ return (5*numHouses +1);
+};
+
+let numMatchsticks = matchHouses(1);
+
+console.log(` number of matchsticks required ${numMatchsticks}`);
+
+numMatchsticks = matchHouses(4);
+console.log(` number of matchsticks required ${numMatchsticks}`);
+
+ numMatchsticks = matchHouses(87);
+
+ console.log(` number of matchsticks required ${numMatchsticks}`);
